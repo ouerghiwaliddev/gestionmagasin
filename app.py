61c5bd313dffc379047d9bd90a6a0b8fc9d1a5de
HP <ouerghi.walid.dev@gmail.com>
2026-07-13 17:56:45 +0100

    change host

--- app.py
+++ app.py
@@ -443,4 +443,4 @@ def internal_error(error):
 
 if __name__ == '__main__':
     # debug=True pour le développement, mettre à False en production
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
